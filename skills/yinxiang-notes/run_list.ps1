64ef0dce56c0176e9108ef8cc8a85c3daf0e784d
ruiyongwang <127424769+ruiyongwang@users.noreply.github.com>
2026-04-03 01:10:18 +0800

    Add skills\yinxiang-notes\run_list.ps1

---
+++ skills/yinxiang-notes/run_list.ps1
@@ -0,0 +1,4 @@
+$env:EVERNOTE_TOKEN="S=s11:U=2bfbb1:E=19d56b38c07:C=19d32a705a0:P=1cd:A=en-devtoken:V=2:H=17fcb643321d9b6df99336b1cff8a758"
+$env:EVERNOTE_NOTESTORE_URL="https://app.yinxiang.com/shard/s11/notestore"
+cd "D:\oclaw\workspace\skills\yinxiang-notes\scripts"
+C:\Users\wry08\AppData\Local\Programs\Python\Python313\python.exe list_notebooks.py
